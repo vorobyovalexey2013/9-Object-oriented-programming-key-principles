6e2917657953acbb52e6b4bf602cfe62402f4fdb
Alexey <valeskey1988@mail.ru>
2023-03-13 20:51:45 +0500

    first commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+}
+
